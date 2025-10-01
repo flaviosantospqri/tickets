b114a246a9f856a8a8f83a4d158c8d3d6b97e721
Flavio Santos <flavio.pasantos@outlook.com>
2025-10-01 18:10:39 -0300

    fix: api link

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "http://localhost:3000",
+   baseURL: "https://ticket-api-197503629071.us-central1.run.app",
 });
 
 export default api;
